93c1b040ec1af79943c38a1edee186dea7b078e8
antoniovillena <espineter@yahoo.com>
2014-06-18 06:06:03 +0200

    Hack para mostrar sólo el primer capítulo

--- Make.bat
+++ Make.bat
@@ -1,4 +1,10 @@
-makeindex "A0.Mi Proyecto fin de carrera.idx"
-bibtex    "A0.Mi Proyecto fin de carrera.aux"
-pdflatex  "A0.Mi Proyecto fin de carrera.tex"
-copy /y   "A0.Mi Proyecto fin de carrera.pdf" "c:\Users\Yo\Google Drive"
+if 0==1 (
+  makeindex "A0.Mi Proyecto fin de carrera.idx"
+  bibtex    "A0.Mi Proyecto fin de carrera.aux"
+  pdflatex  "A0.Mi Proyecto fin de carrera.tex"
+  copy /y   "A0.Mi Proyecto fin de carrera.pdf" "c:\Users\Yo\Google Drive"
+)
+else (
+  pdflatex  "Capitulo01.tex"
+  copy /y   "Capitulo01.pdf" "c:\Users\Yo\Google Drive"
+)
