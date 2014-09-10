5aad931713b10e3aa0bff3fdfd4582107de46baa
antoniovillena <espineter@yahoo.com>
2014-09-10 22:52:10 +0200

    Corregido capítulo 3 y añadidos dos gráficos más

--- Make.bat
+++ Make.bat
@@ -2,9 +2,9 @@ if 0==1 (
   makeindex "A0.Mi Proyecto fin de carrera.idx"
   bibtex    "A0.Mi Proyecto fin de carrera.aux"
   pdflatex  "A0.Mi Proyecto fin de carrera.tex"
-  copy /y   "A0.Mi Proyecto fin de carrera.pdf" "c:\Users\Yo\Google Drive"
+  copy /y   "A0.Mi Proyecto fin de carrera.pdf" "c:\Users\Yo\Google Drive\PFC-AntonioJoseVillena"
 )
 else (
   pdflatex  "Capitulo04.tex"
-  copy /y   "Capitulo04.pdf" "c:\Users\Yo\Google Drive"
+  copy /y   "Capitulo04.pdf" "c:\Users\Yo\Google Drive\PFC-AntonioJoseVillena"
 )
