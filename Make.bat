bb04d1288545a79a3dacb4d3a3c345c04fb3933f
antoniovillena <espineter@yahoo.com>
2014-09-11 14:46:37 +0200

    Acabado capítulo 4 y compilado el pdf general

--- Make.bat
+++ Make.bat
@@ -1,10 +1,9 @@
-if 0==1 (
+if 1==1 (
   makeindex "A0.Mi Proyecto fin de carrera.idx"
   bibtex    "A0.Mi Proyecto fin de carrera.aux"
   pdflatex  "A0.Mi Proyecto fin de carrera.tex"
   copy /y   "A0.Mi Proyecto fin de carrera.pdf" "c:\Users\Yo\Google Drive\PFC-AntonioJoseVillena"
-)
-else (
+) else (
   pdflatex  "Capitulo04.tex"
   copy /y   "Capitulo04.pdf" "c:\Users\Yo\Google Drive\PFC-AntonioJoseVillena"
 )
