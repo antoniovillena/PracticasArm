b01ec7c63e00b9676fab2560b872ce86f0b54daa
antoniovillena <espineter@yahoo.com>
2014-10-01 01:57:37 +0200

    Añado imágenes y avanzo en el capítulo 5

--- ejemplos/inter1.s
+++ ejemplos/inter1.s
@@ -1,55 +1,72 @@
-# gpio2  led1
-# gpio3  led2
-# gpio17 led3
-# gpio22 led4
-# gpio10 led5
-# gpio11 led6
-# gpio4  buzzer
-# gpio21 button1
-# gpio9  button2
-
         .set    GPBASE,   0x20200000
         .set    GPFSEL0,        0x00
         .set    GPFSEL1,        0x04
         .set    GPFSEL2,        0x08
         .set    GPSET0,         0x1c
         .set    GPCLR0,         0x28
-        .set    TIMERBASE,0x2000B400
-        .set    TIMER_CTL,      0x08
-        .set    TIMER_CNT,      0x20
-
+        .set    STBASE,   0x20003000
+        .set    STCS,           0x00
+        .set    STCLO,          0x04
+        .set    STC1,           0x10
+        .set    INTBASE,  0x2000b000
+        .set    INTENIRQ1,     0x210
+.text
+        mov     r0, #0x18     @IRQ vector
+        ldr     r1, =irq_handler
+        bl      add_exception
+        mov     r0, #0xd2     @IRQ mode, FIQ&IRQ disable
+        msr     cpsr_c, r0
+        mov     sp, #0x8000
+        mov     r0, #0xd3     @SVC mode, FIQ&IRQ disable
+        msr     cpsr_c, r0
+        mov     sp, #0x8000000
         ldr     r0, =GPBASE
-        ldr     r1, =0b00000000000000000001001001000000
+        mov     r1, #0b00000000000000000000001001000000
         str     r1, [r0, #GPFSEL0]
         ldr     r1, =0b00000000001000000000000000001001
         str     r1, [r0, #GPFSEL1]
         mov     r1, #0b00000000000000000000000001000000
         str     r1, [r0, #GPFSEL2]
-        ldr     r2, =TIMERBASE
-        ldr     r3, =0x00f90200
-        str     r3, [r2, #TIMER_CTL]
 
-bucle:  ldr     r1, =0b00000000000000000000100
-        bl      secuen
-        ldr     r1, =0b00000000000000000001000
-        bl      secuen
-        ldr     r1, =0b00000100000000000000000
-        bl      secuen
-        ldr     r1, =0b10000000000000000000000
-        bl      secuen
-        ldr     r1, =0b00000000000010000000000
-        bl      secuen
-        ldr     r1, =0b00000000000100000000000
-        bl      secuen
-        b       bucle
 
-secuen: str     r1, [r0, #GPSET0]
-        ldr     r3, [r2, #TIMER_CNT]
-        ldr     r4, =200000
-        add     r4, r3
-ret1:   ldr     r3, [r2, #TIMER_CNT]
-        cmp     r3, r4
-        bne     ret1
-        ldr     r1, =0b00000000010000100000110000001100
-        str     r1, [r0, #GPCLR0]
+        ldr     r0, =STBASE
+        ldr     r1, [r0, #STCLO]
+        add     r1, #1
+        str     r1, [r0, #STC1]
+
+        ldr     r0, =INTBASE
+        mov     r1, #0b0010
+        str     r1, [r0, #INTENIRQ1]
+        mov     r0, #0x53     @SVC mode, IRQ enable
+        msr     cpsr_c, r0
+bucle:  b       bucle
+
+add_exception:
+        sub     r1, r0
+        lsr     r1, #2
+        sub     r1, #2
+        orr     r1, #0xea000000
+        str     r1, [r0]
         bx      lr
+
+irq_handler:
+        push    {r0, r1, r2}
+        ldr     r0, =ledst
+        ldr     r1, [r0]
+        eors    r1, #1
+        str     r1, [r0]
+        ldr     r0, =GPBASE
+        ldr     r1, =0b00000000010000100000110000001100
+        streq   r1, [r0, #GPSET0]
+        strne   r1, [r0, #GPCLR0]
+        ldr     r0, =STBASE
+        ldr     r1, [r0, #STCLO]
+        ldr     r2, =500000       @1 Hz
+        add     r1, r2
+        str     r1, [r0, #STC1]
+        mov     r1, #0b0010
+        str     r1, [r0, #STCS]
+        pop     {r0, r1, r2}
+        subs    pc, lr, #4
+
+ledst:  .word   0
--- ejemplos/inter2.s
+++ ejemplos/inter2.s
@@ -1,13 +1,3 @@
-# gpio2  led1
-# gpio3  led2
-# gpio17 led3
-# gpio22 led4
-# gpio10 led5
-# gpio11 led6
-# gpio4  buzzer
-# gpio21 button1
-# gpio9  button2
-
         .set    GPBASE,   0x20200000
         .set    GPFSEL0,        0x00
         .set    GPFSEL1,        0x04
@@ -17,9 +7,9 @@
         .set    STBASE,   0x20003000
         .set    STCS,           0x00
         .set    STCLO,          0x04
-        .set    STC0,           0x0c
+        .set    STC1,           0x10
+        .set    STC3,           0x18
         .set    INTBASE,  0x2000b000
-        .set    INTIRQP1,      0x204
         .set    INTENIRQ1,     0x210
 .text
         mov     r0, #0x18     @IRQ vector
@@ -32,14 +22,21 @@
         msr     cpsr_c, r0
         mov     sp, #0x8000000
         ldr     r0, =GPBASE
-        ldr     r1, =0b00000000000000000001001001000000
+        mov     r1, #0b00000000000000000001001001000000
         str     r1, [r0, #GPFSEL0]
         ldr     r1, =0b00000000001000000000000000001001
         str     r1, [r0, #GPFSEL1]
         mov     r1, #0b00000000000000000000000001000000
         str     r1, [r0, #GPFSEL2]
+
+        ldr     r0, =STBASE
+        ldr     r1, [r0, #STCLO]
+        add     r1, #1
+        str     r1, [r0, #STC1]
+        str     r1, [r0, #STC3]
+
         ldr     r0, =INTBASE
-        mov     r1, #1
+        mov     r1, #0b1010
         str     r1, [r0, #INTENIRQ1]
         mov     r0, #0x53     @SVC mode, IRQ enable
         msr     cpsr_c, r0
@@ -54,21 +51,54 @@ add_exception:
         bx      lr
 
 irq_handler:
-        push    {r0, r1, r2}
-        ldr     r0, =ledst
-        ldr     r1, [r0]
-        eors    r1, #1
-        str     r1, [r0]
-        ldr     r0, =GPBASE
-        ldr     r1, =0b00000000010000100000110000001100
-        streq   r1, [r0, #GPSET0]
-        strne   r1, [r0, #GPCLR0]
+        push    {r0, r1, r2, r3}
         ldr     r0, =STBASE
-        ldr     r1, [r0, #STCLO]
-        ldr     r2, =500000
-        add     r1, r2
-        str     r1, [r0, #STC0]
-        pop     {r0, r1, r2}
+        ldr     r1, =GPBASE
+        ldr     r2, [r0, #STCS]
+        ands    r2, #0b0010
+        beq     sonido
+
+        ldr     r2, =cuenta
+        ldr     r3, =0b00000000010000100000110000001100
+        str     r3, [r1, #GPCLR0]
+        ldr     r3, [r2]          @cuenta
+        subs    r3, #1
+        movmi   r3, #5
+        str     r3, [r2], #4      @cuenta
+        ldr     r3, [r2, +r3, LSL #2]
+        str     r3, [r1, #GPSET0]
+        ldr     r3, [r0, #STCLO]
+        ldr     r2, =200000       @5 Hz
+        add     r3, r2
+        str     r3, [r0, #STC1]
+        mov     r3, #0b0010
+        str     r3, [r0, #STCS]
+        ldr     r3, [r0, #STCS]
+        ands    r3, #0b0100
+        beq     final
+
+sonido: ldr     r2, =bitson
+        ldr     r3, [r2]
+        eors    r3, #1
+        str     r3, [r2]
+        mov     r3, #0b10000      @altavoz
+        streq   r3, [r1, #GPSET0]
+        strne   r3, [r1, #GPCLR0]
+        ldr     r3, [r0, #STCLO]
+        ldr     r2, =1136         @440 Hz
+        add     r3, r2
+        str     r3, [r0, #STC3]
+        mov     r3, #0b1000
+        str     r3, [r0, #STCS]
+final:  pop     {r0, r1, r2, r3}
         subs    pc, lr, #4
 
-ledst:  .word   0
+bitson: .word   0
+cuenta: .word   0
+secuen: .word   0b00000000000000000000100
+        .word   0b00000000000000000001000
+        .word   0b00000100000000000000000
+        .word   0b10000000000000000000000
+        .word   0b00000000000010000000000
+        .word   0b00000000000100000000000
+
