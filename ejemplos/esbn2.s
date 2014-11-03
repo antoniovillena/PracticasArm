e9b445c7e61e467f68334a9335b1c8c7f2fd8641
antoniovillena <espineter@yahoo.com>
2014-11-03 12:16:52 +0100

    Acabado capítulo 4

--- ejemplos/esbn2.s
+++ ejemplos/esbn2.s
@@ -1,7 +1,6 @@
         .set    GPBASE,   0x20200000
         .set    GPFSEL0,  0x00
         .set    GPSET0,   0x1c
-        .set    GPCLR0,   0x28
 .text
         ldr     r0, =GPBASE
         ldr     r1, =0b00000000000000000000001001001001
