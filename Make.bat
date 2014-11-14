efc6cbbf084a12ca3f9ccc958a063775d3aefee0
antoniovillena <espineter@yahoo.com>
2014-11-14 18:03:34 +0100

    Renombrado para quitar tilde y quito la carpeta code que no hace nada

--- Make.bat
+++ Make.bat
@@ -1,9 +1,9 @@
-if 0==1 (
   makeindex "A0.Mi Proyecto fin de carrera.idx"
   bibtex    "A0.Mi Proyecto fin de carrera.aux"
   pdflatex  "A0.Mi Proyecto fin de carrera.tex"
   copy /y   "A0.Mi Proyecto fin de carrera.pdf" "c:\Users\Yo\Google Drive\PFC-AntonioJoseVillena"
-) else (
-  pdflatex  "Capitulo05.tex"
-  copy /y   "Capitulo05.pdf" "c:\Users\Yo\Google Drive\PFC-AntonioJoseVillena"
-)
+
+  makeindex "A_.Libro de Practicas.idx"
+  bibtex    "A_.Libro de Practicas.aux"
+  pdflatex  "A_.Libro de Practicas.tex"
+  copy /y   "A_.Libro de Practicas.pdf" "c:\Users\Yo\Google Drive\PFC-AntonioJoseVillena"
--- limpia.bat
+++ limpia.bat
@@ -20,6 +20,35 @@ del "A0.Mi Proyecto fin de carrera.aux" ^
     "A0.Mi Proyecto fin de carrera.mtc8" ^
     "A0.Mi Proyecto fin de carrera.mtc9" ^
     "A0.Mi Proyecto fin de carrera.mtc10" ^
+    "A0.Mi Proyecto fin de carrera.mtc11" ^
+    "A0.Mi Proyecto fin de carrera.mtc12" ^
+    "A0.Mi Proyecto fin de carrera.mtc13" ^
     "A0.Mi Proyecto fin de carrera.out" ^
     "A0.Mi Proyecto fin de carrera.pdf" ^
     "A0.Mi Proyecto fin de carrera.toc"
+
+del "A_.Libro de Practicas.aux" ^
+    "A_.Libro de Practicas.idx" ^
+    "A_.Libro de Practicas.bbl" ^
+    "A_.Libro de Practicas.blg" ^
+    "A_.Libro de Practicas.ilg" ^
+    "A_.Libro de Practicas.ind" ^
+    "A_.Libro de Practicas.lof" ^
+    "A_.Libro de Practicas.log" ^
+    "A_.Libro de Practicas.lot" ^
+    "A_.Libro de Practicas.maf" ^
+    "A_.Libro de Practicas.mtc" ^
+    "A_.Libro de Practicas.mtc0" ^
+    "A_.Libro de Practicas.mtc1" ^
+    "A_.Libro de Practicas.mtc2" ^
+    "A_.Libro de Practicas.mtc3" ^
+    "A_.Libro de Practicas.mtc4" ^
+    "A_.Libro de Practicas.mtc5" ^
+    "A_.Libro de Practicas.mtc6" ^
+    "A_.Libro de Practicas.mtc7" ^
+    "A_.Libro de Practicas.mtc8" ^
+    "A_.Libro de Practicas.mtc9" ^
+    "A_.Libro de Practicas.mtc10" ^
+    "A_.Libro de Practicas.out" ^
+    "A_.Libro de Practicas.pdf" ^
+    "A_.Libro de Practicas.toc"
