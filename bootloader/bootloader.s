4e08e651bbd8439da96b9275dbdcf05ec6f2cce5
antoniovillena <espineter@yahoo.com>
2014-11-19 13:32:17 +0100

    Pequeña optimización

--- bootloader/bootloader.s
+++ bootloader/bootloader.s
@@ -11,62 +11,56 @@
         .set    GPPUD,      0x94
         .set    GPPUDCLK0,  0x98
         state   .req    r0
-        recv    .req    r1
-        send    .req    r1
-        gpbas   .req    r2
-        auxbas  .req    r2
-        block   .req    r3
-        addr    .req    r4
-        crc     .req    r5
-        counter .req    r6
-        cnt     .req    r7
-        timeout .req    r8
+        recv    .req    r3
+        send    .req    r3
+        gpbas   .req    r4
+        auxbas  .req    r4
+        block   .req    r5
+        addr    .req    r6
+        crc     .req    r7
+        cntadr  .req    r8
+        cnt     .req    r9
+        timeout .req    r10
+        sel1bak .req    r11
 .text
-inicio: mov     sp, #0x8000
-        mov     addr, #0x8200
-        push    {r0-r2}
-inici1: ldr     r1, [addr], #-4
-        str     r1, [addr, #-4092]
+start:  mov     addr, #0x8200
+star1:  ldr     r3, [addr], #-4
+        str     r3, [addr, #-4092]
         cmp     addr, #0x8000
-        bne     inici1
-        b       start-0x1000
-start:  ldr     gpbas, =GPBASE
-        ldr     r5, [gpbas, #GPFSEL1]
-        mov     r1, #0b00000000000000010010000000000000
-        str     r1, [gpbas, #GPFSEL1]
-        mov     r1, #2
-        str     r1, [gpbas, #GPPUD]
+        bne     star1
+        ldr     gpbas, =GPBASE
+        sub     cntadr, gpbas, #GPBASE-0x20004000
+        ldr     sel1bak, [gpbas, #GPFSEL1]
+        mov     r3, #0b00000000000000010010000000000000
+        str     r3, [gpbas, #GPFSEL1]
+        mov     r3, #2
+        str     r3, [gpbas, #GPPUD]
         bl      wait
-        mov     r1, #0b0000000000010000
-        str     r1, [gpbas, #GPPUDCLK0]
+        mov     r3, #0b0000000000010000
+        str     r3, [gpbas, #GPPUDCLK0]
         bl      wait
         str     r0, [gpbas, #GPPUDCLK0]
-        ldr     r1, [gpbas, #GPLEV0]
-        tst     r1, #0b00000010000
-        beq     star2
-        str     r5, [gpbas, #GPFSEL1]
-star1:  ldr     r1, [addr, #final+8-inicio]
-        str     r1, [addr], #4
-        cmp     addr, #0x330000
-        bne     star1
-        b       star5
-star2:  add     auxbas, #AUXBASE-GPBASE
+        ldr     r3, [gpbas, #GPLEV0]
+        tst     r3, #0b00000010000
+        bne     linux-0x1000
+        add     auxbas, #AUXBASE-GPBASE
         mov     block, #1
         str     block, [auxbas, #AMENABLES]
-        mov     r1, #0x23
-        str     r1, [auxbas, #AMLCRREG]
-        add     r1, #270-0x23
-        str     r1, [auxbas, #AMBAUDREG]
-        ldr     counter, =STCLO
-        ldr     cnt, [counter]
-star3:  add     timeout, cnt, #0xa000
-        mov     send, #0x15
-        str     send, [auxbas, #AMIOREG]
-star4:  ldr     cnt, [counter]
+        mov     timeout, #0x23
+        str     timeout, [auxbas, #AMLCRREG]
+        add     r3, timeout, #270-0x23
+        str     r3, [auxbas, #AMBAUDREG]
+        b       star4-0x1000
+star2:  uadd8   crc, crc, recv
+        strb    recv, [addr], #1
+star3:  add     state, #1
+star4:  ldr     cnt, [cntadr, #STCLO-0x20004000]
         cmp     cnt, timeout
-        bcs     star3
-        ldr     r1, [auxbas, #AMLSRREG]
-        tst     r1, #1
+        addcs   timeout, cnt, #0xa000
+        movcs   send, #0x15
+        strcs   send, [auxbas, #AMIOREG]
+        ldr     r3, [auxbas, #AMLSRREG]
+        tst     r3, #1
         beq     star4
         ldr     recv, [auxbas, #AMIOREG]
         add     timeout, cnt, #0xa000
@@ -74,19 +68,19 @@ star4:  ldr     cnt, [counter]
         bcs     star6
         cmp     recv, #0x01
         moveq   crc, #0
-        beq     starc
+        beq     star3
         cmp     recv, #0x04
         bne     star8
         mov     send, #0x06
         str     send, [auxbas, #AMIOREG]
 star5:  subs    addr, #1
         bne     star5
-        pop     {r0-r2}
-        b       inicio+0x1000
+        str     sel1bak, [gpbas, #GPFSEL1]
+        b       start+0x1000
 star6:  bne     stara
         mov     cnt, block
 star7:  cmp     recv, cnt
-        beq     starc
+        beq     star3
 star8:  mov     send, #0x15
 star9:  mov     state, #0
         str     send, [auxbas, #AMIOREG]
@@ -95,19 +89,20 @@ stara:  cmp     state, #2
         eoreq   cnt, block, #0xff
         beq     star7
         cmp     state, #131
-        bne     starb
+        bne     star2
         cmp     recv, crc
         bne     star8
-        mov     r1, #1
-        uadd8   block, block, r1
+        mov     r3, #1
+        uadd8   block, block, r3
         mov     send, #0x06
         b       star9
-starb:  uadd8   crc, crc, recv
-        strb    recv, [addr], #1
-starc:  add     state, #1
-        b       star4
-wait:   mov     r1, #50
-wait1:  subs    r1, #1
+wait:   mov     r3, #50
+wait1:  subs    r3, #1
         bne     wait1
         bx      lr
+linux:  ldr     r3, [addr, #final+4-start]
+        str     r3, [addr], #4
+        cmp     addr, #0x330000
+        bne     linux
+        b       star5
 final:
