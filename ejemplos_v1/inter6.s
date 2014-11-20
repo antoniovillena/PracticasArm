4cda143f35b47cc2d449821421b976a93a56eced
antoniovillena <espineter@yahoo.com>
2014-11-20 22:07:32 +0100

    Desplazo para dejar hueco al ejemplo inter3.s

--- ejemplos_v1/inter6.s
+++ ejemplos_v1/inter6.s
@@ -19,19 +19,34 @@
         str     r1, [r0, #GPFSEL1]
         mov     r1, #0b00000000000000000000000001000000
         str     r1, [r0, #GPFSEL2]
+        mov     r1, #0b00000000000000000000101
+        str     r1, [r0, #GPSET0]
+        mov     r1, #2
+        str     r1, [r0, #GPPUD]
+        bl      wait
+        ldr     r1, =0b00001000001000000000001000000000
+        str     r1, [r0, #GPPUDCLK0]
+        bl      wait
+        mov     r2, #0
+        str     r2, [r0, #GPPUD]
+        str     r1, [r0, #GPREN0]
         ldr     r0, =STBASE
         ldr     r1, [r0, #STCLO]
         add     r1, #2
         str     r1, [r0, #STC1]
-        str     r1, [r0, #STC3]
         ldr     r0, =INTBASE
-        mov     r1, #0b0010
-        str     r1, [r0, #INTENIRQ1]
-        mov     r1, #0x80 | 3
+        mov     r1, #0b00000000000100000000000000000000
+        str     r1, [r0, #INTENIRQ2]
+        mov     r1, #0x80 | 1
         str     r1, [r0, #INTFIQCON]
         mov     r0, #0x13     @modo SVC, FIQ&IRQ activo
         msr     cpsr_c, r0
 bucle:  b       bucle
+
+wait:   mov     r2, #50
+wait1:  subs    r2, #1
+        bne     wait1
+        bx      lr
 
 fiq_handler:
         ldr     r8, =GPBASE
@@ -45,11 +60,11 @@ fiq_handler:
         streq   r10, [r8, #GPSET0]
         strne   r10, [r8, #GPCLR0]
         ldr     r8, =STBASE
-        mov     r10, #0b1000
+        mov     r10, #0b0010
         str     r10, [r8, #STCS]
         ldr     r10, [r8, #STCLO]
         add     r10, r9
-        str     r10, [r8, #STC3]
+        str     r10, [r8, #STC1]
         subs    pc, lr, #4
 
 irq_handler:
@@ -58,24 +73,28 @@ irq_handler:
         ldr     r1, =cuenta
         ldr     r2, =0b00000000010000100000110000001111
         str     r2, [r0, #GPCLR0]
+        ldr     r2, [r0, #GPEDS0]
+        ands    r2, #0b00000000000000000000001000000000
+        beq     incre
+        str     r2, [r0, #GPEDS0]
         ldr     r2, [r1]        @cuenta
         subs    r2, #1
         movmi   r2, #5
-        str     r2, [r1], #4    @cuenta
+        b       conti
+incre:  mov     r2, #0b00001000001000000000000000000000
+        str     r2, [r0, #GPEDS0]
+        ldr     r2, [r1]        @cuenta
+        add     r2, #1
+        cmp     r2, #6
+        moveq   r2, #0
+conti:  str     r2, [r1], #4    @cuenta
         ldr     r2, [r1, +r2, LSL #3]
         str     r2, [r0, #GPSET0]
-        ldr     r0, =STBASE
-        mov     r2, #0b0010
-        str     r2, [r0, #STCS]
-        ldr     r2, [r0, #STCLO]
-        ldr     r1, =500000     @2 Hz
-        add     r2, r1
-        str     r2, [r0, #STC1]
         pop     {r0, r1, r2}
         subs    pc, lr, #4
 
 bitson: .word   0
-cuenta: .word   0
+cuenta: .word   5
 secuen: .word   0b00000000000100000000000
         .word   716
         .word   0b00000000000010000000000
