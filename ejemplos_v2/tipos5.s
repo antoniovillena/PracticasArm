f636316ab159c70ec3bb9c0b0721abf70e6a9afa
antoniovillena <espineter@yahoo.com>
2014-12-16 11:15:27 +0100

    Actualizo hasta el copítulo 2

--- ejemplos_v2/tipos5.s
+++ ejemplos_v2/tipos5.s
@@ -6,15 +6,24 @@ var2:   .word   1600000000, -100, 800000000, -50, 200
 .text
 .global main
 
+/* salvamos registros */
 main:   push    {r4, lr}
+
+/* inicializamos variables y apuntamos r2 a var2 */
         mov     r0, #5
         mov     r1, #0
         ldr     r2, =var2
+
+/* bucle que hace la suma */
 bucle:  ldr     r3, [r2], #4
         add     r1, r1, r3
         subs    r0, r0, #1
         bne     bucle
+
+/* imprimimos resultado */
         ldr     r0, =var1
         bl      printf
+
+/* recuperamos registros y salimos */
         pop     {r4, lr}
         bx      lr
