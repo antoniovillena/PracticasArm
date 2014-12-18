46dbfb655ebeafc17e78728d991513a8debc40a6
antoniovillena <espineter@yahoo.com>
2014-12-18 16:01:46 +0100

    Acabado lo que me faltaba del capítulo 5. Ya sólo me falta revisar el PDF de Francisco

--- ejemplos_v2/inter3.s
+++ ejemplos_v2/inter3.s
@@ -1,41 +1,54 @@
         .include  "inter.inc"
 .text
+/* Agrego vector interrupción */
         ADDEXC  0x18, irq_handler
-        mov     r0, #0b11010010   @modo IRQ, FIQ&IRQ desact
+
+/* Inicializo la pila en modos IRQ y SVC */
+        mov     r0, #0b11010010   @ Modo IRQ, FIQ&IRQ desact
         msr     cpsr_c, r0
         mov     sp, #0x8000
-        mov     r0, #0b11010011   @modo SVC, FIQ&IRQ desact
+        mov     r0, #0b11010011   @ Modo SVC, FIQ&IRQ desact
         msr     cpsr_c, r0
         mov     sp, #0x8000000
+
+/* Configuro GPIOs 9 y 10 como salida */
         ldr     r0, =GPBASE
-        ldr     r1, =0b00001000000000000000000000000000
+        mov     r1, #0b00001000000000000000000000000000
         str     r1, [r0, #GPFSEL0]
 /* guia bits           xx999888777666555444333222111000*/
-        ldr     r1, =0b00000000000000000000000000000001
+        mov     r1, #0b00000000000000000000000000000001
         str     r1, [r0, #GPFSEL1]
-/* guia bits           10987654321098765432109876543210*/
+
+/* Enciendo LEDs       10987654321098765432109876543210*/
         mov     r1, #0b00000000000000000000011000000000
         str     r1, [r0, #GPSET0]
+
+/* Habilito pines GPIO 2 y 3 (botones) para interrupciones*/
         mov     r1, #0b00000000000000000000000000001100
         str     r1, [r0, #GPREN0]
         ldr     r0, =INTBASE
-/* guia bits           10987654321098765432109876543210*/
+
+/* Habilito interrupciones, local y globalmente */
         mov     r1, #0b00000000000100000000000000000000
+/* guia bits           10987654321098765432109876543210*/
         str     r1, [r0, #INTENIRQ2]
-        mov     r0, #0b01010011   @modo SVC, IRQ activo
+        mov     r0, #0b01010011   @ Modo SVC, IRQ activo
         msr     cpsr_c, r0
+
+/* Repetir para siempre */
 bucle:  b       bucle
 
+/* Rutina de tratamiento de interrupción */
 irq_handler:
         push    {r0, r1}
         ldr     r0, =GPBASE
-/* Apaga los dos LEDs rojos  54321098765432109876543210*/
+/* Apago los dos LEDs rojos  54321098765432109876543210*/
         mov     r1, #0b00000000000000000000011000000000
         str     r1, [r0, #GPCLR0]
-/* Consulta si se ha pulsado el botón GPIO2 */
+/* Consulto si se ha pulsado el botón GPIO2 */
         ldr     r1, [r0, #GPEDS0]
         ands    r1, #0b00000000000000000000000000000100
-/* Sí: Activa GPIO 9; No: Activa GPIO 10 */
+/* Sí: Activo GPIO 9; No: Activo GPIO 10 */
         movne   r1, #0b00000000000000000000001000000000
         moveq   r1, #0b00000000000000000000010000000000
         str     r1, [r0, #GPSET0]
--- ejemplos_v2/inter4.s
+++ ejemplos_v2/inter4.s
@@ -1,12 +1,17 @@
         .include  "inter.inc"
 .text
+/* Agrego vector interrupción */
         ADDEXC  0x18, irq_handler
-        mov     r0, #0b11010010   @modo IRQ, FIQ&IRQ desact
+
+/* Inicializo la pila en modos IRQ y SVC */
+        mov     r0, #0b11010010   @ Modo IRQ, FIQ&IRQ desact
         msr     cpsr_c, r0
         mov     sp, #0x8000
-        mov     r0, #0b11010011   @modo SVC, FIQ&IRQ desact
+        mov     r0, #0b11010011   @ Modo SVC, FIQ&IRQ desact
         msr     cpsr_c, r0
         mov     sp, #0x8000000
+
+/* Configuro GPIOs 4, 9, 10, 11, 17, 22 y 27 como salida */
         ldr     r0, =GPBASE
         ldr     r1, =0b00001000000000000001000000000000
         str     r1, [r0, #GPFSEL0]
@@ -15,62 +20,87 @@
         str     r1, [r0, #GPFSEL1]
         ldr     r1, =0b00000000001000000000000001000000
         str     r1, [r0, #GPFSEL2]
+
+/* Programo C1 y C3 para dentro de 2 microsegundos */
         ldr     r0, =STBASE
         ldr     r1, [r0, #STCLO]
         add     r1, #2
         str     r1, [r0, #STC1]
         str     r1, [r0, #STC3]
+
+/* Habilito interrupciones, local y globalmente */
         ldr     r0, =INTBASE
         mov     r1, #0b1010
         str     r1, [r0, #INTENIRQ1]
-        mov     r0, #0b01010011   @modo SVC, IRQ activo
+        mov     r0, #0b01010011   @ Modo SVC, IRQ activo
         msr     cpsr_c, r0
+
+/* Repetir para siempre */
 bucle:  b       bucle
 
+/* Rutina de tratamiento de interrupción */
 irq_handler:
         push    {r0, r1, r2, r3}
+
+/* Leo origen de la interrupción */
         ldr     r0, =STBASE
         ldr     r1, =GPBASE
         ldr     r2, [r0, #STCS]
         ands    r2, #0b0010
         beq     sonido
+
+/* Si es C1, ejecuto secuencia de LEDs */
         ldr     r2, =cuenta
 /* guia bits           10987654321098765432109876543210*/
         ldr     r3, =0b00001000010000100000111000000000
-        str     r3, [r1, #GPCLR0]
-        ldr     r3, [r2]          @cuenta
-        subs    r3, #1
-        moveq   r3, #6
-        str     r3, [r2]
-        ldr     r3, [r2, +r3, LSL #2]
-        str     r3, [r1, #GPSET0]
+        str     r3, [r1, #GPCLR0] @ Apago todos los LEDs
+        ldr     r3, [r2]          @ Leo variable cuenta
+        subs    r3, #1            @ Decremento
+        moveq   r3, #6            @ Si es 0, volver a 6
+        str     r3, [r2]          @ Escribo cuenta
+        ldr     r3, [r2, +r3, LSL #2] @ Leo secuencia
+        str     r3, [r1, #GPSET0] @ Escribo secuencia en LEDs
+
+/* Reseteo estado interrupción de C1 */
         mov     r3, #0b0010
         str     r3, [r0, #STCS]
+
+/* Programo siguiente interrupción en 200ms */
         ldr     r3, [r0, #STCLO]
-        ldr     r2, =200000       @5 Hz
+        ldr     r2, =200000       @ 5 Hz
         add     r3, r2
         str     r3, [r0, #STC1]
+
+/* ¿Hay interrupción pendiente en C3? */
         ldr     r3, [r0, #STCS]
         ands    r3, #0b0100
-        beq     final
+        beq     final             @ Si no, salgo
+
+/* Si es C3, hago sonar altavoz
 sonido: ldr     r2, =bitson
         ldr     r3, [r2]
-        eors    r3, #1
+        eors    r3, #1            @ Invierto estado
         str     r3, [r2]
-        mov     r3, #0b10000      @altavoz
-        streq   r3, [r1, #GPSET0]
-        strne   r3, [r1, #GPCLR0]
+        mov     r3, #0b10000      @ GPIO 4 (altavoz)
+        streq   r3, [r1, #GPSET0] @ Escribo en altavoz
+        strne   r3, [r1, #GPCLR0] @ Escribo en altavoz
+
+/* Reseteo estado interrupción de C3 */
         mov     r3, #0b1000
         str     r3, [r0, #STCS]
+
+/* Programo interrupción para sonido de 440 Hz */
         ldr     r3, [r0, #STCLO]
-        ldr     r2, =1136         @440 Hz
+        ldr     r2, =1136         @ Contador para 440 Hz
         add     r3, r2
         str     r3, [r0, #STC3]
+
+/* Recupero registros y salgo */
 final:  pop     {r0, r1, r2, r3}
         subs    pc, lr, #4
 
-bitson: .word   0
-cuenta: .word   1
+bitson: .word   0             @ Bit 0 = Estado del altavoz
+cuenta: .word   1             @ Entre 1 y 6, LED a encender
 /* guia bits      7654321098765432109876543210*/
 secuen: .word   0b1000000000000000000000000000
         .word   0b0000010000000000000000000000
