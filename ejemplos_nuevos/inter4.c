8c2dc5bf552e2bb18838afbb0a3976d0972125bb
antoniovillena <espineter@yahoo.com>
2015-04-11 12:34:09 +0200

    Añado send y modifico Makefile para que funcione en Windows con MSYS y Yagarto

--- ejemplos_nuevos/inter4.c
+++ ejemplos_nuevos/inter4.c
@@ -1,20 +1,21 @@
 #include  "const.h"
 
 void irq_handler();
-int cuenta= 5, bitson= 0;
+int i, cuenta, bitson;
 unsigned char leds[6]= {L1,L2,L3,L4,L5,L6};
 
 int notmain(){
   addexc(IRQVECT, irq_handler);
   putsp(IRQMODE, 0x8000);
   putsp(SVCMODE, 0x8000000);
-  while ( bitson < 6 )
-    gpio_fsel(leds[bitson++], OUTPUT);
+  for ( i= 0; i<6; i++ )
+    gpio_fsel(leds[i], OUTPUT);
   gpio_fsel(ALT, OUTPUT);
   systim_add(2, 1);
   systim_add(2, 3);
   irq_enable(C1INT);
   irq_enable(C3INT);
+  cuenta= 5;
   int_globalenable(IRQ);
   while(1);
 }
--- ejemplos_nuevos/inter5.c
+++ ejemplos_nuevos/inter5.c
@@ -2,7 +2,7 @@
 
 void irq_handler();
 void fiq_handler();
-int cuenta= 5, bitson= 0;
+int i, cuenta, bitson;
 unsigned char leds[6]= {L1,L2,L3,L4,L5,L6};
 unsigned short freq[6]= {1136, 1012, 956, 851, 758, 716};
 
@@ -12,13 +12,14 @@ int notmain(){
   putsp(FIQMODE, 0x4000);
   putsp(IRQMODE, 0x8000);
   putsp(SVCMODE, 0x8000000);
-  while ( bitson < 6 )
-    gpio_fsel(leds[bitson++], OUTPUT);
+  for ( i= 0; i<6; i++ )
+    gpio_fsel(leds[i], OUTPUT);
   gpio_fsel(ALT, OUTPUT);
   systim_add(2, 1);
   systim_add(2, 3);
   irq_enable(C1INT);
   fiq_enable(C3INT);
+  cuenta= 5;
   int_globalenable(IRQ+FIQ);
   while(1);
 }
